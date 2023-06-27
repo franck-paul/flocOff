2185bdce065a0f147b094d149dac2786a0efec56
franck <carnet.franck.paul@gmail.com>
2023-06-27 09:33:55 +0200

    Release 3.0.1

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'flocOff',
     'Disable Google FLoC tracking',
     'Franck Paul and contributors',
-    '3.0',
+    '3.0.1',
     [
         'requires'    => [['core', '2.26']],
         'permissions' => dcCore::app()->auth->makePermissions([
