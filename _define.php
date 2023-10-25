be11cb9b481f318d9b251e7a09eadfd84261c86a
franck <carnet.franck.paul@gmail.com>
2023-10-25 12:04:44 +0200

    New release 5.0

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'flocOff',
     'Disable Google FLoC tracking',
     'Franck Paul and contributors',
-    '4.0',
+    '5.0',
     [
         'requires'    => [['core', '2.28']],
         'permissions' => 'My',
