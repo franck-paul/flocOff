15f18666fc559566281210b1bb33cb8198895c0c
franck <carnet.franck.paul@gmail.com>
2025-09-07 16:03:07 +0200

    DC 2.36 compliance

--- _define.php
+++ _define.php
@@ -18,7 +18,7 @@ $this->registerModule(
     '5.2',
     [
         'date'        => '2003-08-13T13:42:00+0100',
-        'requires'    => [['core', '2.28']],
+        'requires'    => [['core', '2.36']],
         'permissions' => 'My',
         'type'        => 'plugin',
         'settings'    => [
@@ -28,5 +28,6 @@ $this->registerModule(
         'details'    => 'https://open-time.net/?q=flocOff',
         'support'    => 'https://github.com/franck-paul/flocOff',
         'repository' => 'https://raw.githubusercontent.com/franck-paul/flocOff/main/dcstore.xml',
+        'license'    => 'gpl2',
     ]
 );
